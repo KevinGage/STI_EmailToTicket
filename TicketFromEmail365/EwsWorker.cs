a173a3f5da21083d1ec5d6649bb76b4876f58487
Kevin Gage <dudeman02379@gmail.com>
2015-07-17 10:03:18 -0400

    started work on testing connection to EWS on 365 using API

---
+++ TicketFromEmail365/EwsWorker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Exchange.WebServices.Data;
+
+namespace TicketFromEmail365
+{
+    class EwsWorker
+    {
+        Config _currentConfig;
+
+        public EwsWorker(Config config)
+        {
+            _currentConfig = config;
+
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+
+            service.UseDefaultCredentials = false;
+            service.Credentials = new WebCredentials(_currentConfig.User, _currentConfig.Password);
+
+            if (_currentConfig.LogLevel > 0)
+            {
+                Logger.writeSingleLine("Attempting to login to ");
+            }
+            service.AutodiscoverUrl(_currentConfig.User, RedirectionUrlValidationCallback);
+        }
+
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl) // used as a callback to make sure autodiscover hits a https endpoint
+        {
+            Logger.writeSingleLine("Attempting to resolve Autodiscover URL");
+
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+
+            string logMessage = "Unable to connect to autodiscover address: " + redirectionUrl;
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+                logMessage = "Succesfully resolved autodiscover address: " + redirectionUrl;
+            }
+
+            Logger.writeSingleLine(logMessage);
+            return result;
+        }
+    }
+}
