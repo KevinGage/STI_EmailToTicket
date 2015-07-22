10fba351873775ac7c45c0864f9460584d92f9f1
Kevin Gage <dudeman02379@gmail.com>
2015-07-22 15:07:33 -0400

    created EmailMessage class for received messages

---
+++ TicketFromEmail365/EmailMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketFromEmail365
+{
+    class EmailMessage
+    {
+        string _sender;
+        string _subject;
+        string _body;
+
+        public EmailMessage(string sender, string subject, string body)
+        {
+            _sender = sender;
+            _subject = subject;
+            _body = body;
+        }
+
+        public bool CheckEmailDomain()
+        {
+            //should check database for senders email domain.  Return true if client domain exists. false if domain doesn't belong to a client.
+            return false;
+        }
+
+
+    }
+}
