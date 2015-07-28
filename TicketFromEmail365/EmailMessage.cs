cf4479247e3558cdac58f44d83c947d9daed56ba
Kevin Gage <dudeman02379@gmail.com>
2015-07-28 10:24:25 -0400

    added static bool method to EmailMessages to test database connections #19

--- TicketFromEmail365/EmailMessage.cs
+++ TicketFromEmail365/EmailMessage.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data.SqlClient;
+
 namespace TicketFromEmail365
 {
     class EmailMessage
@@ -30,6 +32,31 @@ namespace TicketFromEmail365
             //This should be triggered by CheckEmailDomain if the message is from a client.
             //First check subject to see if it already includes a ticket number.  If yes verify ticket exists and is open.  Re-open if closed.
             //If no create new ticket in database for appropriate client.  Change current subject to include ticket info
+        }
+
+        public static bool TestDatabaseConnection(Config currentConfig)
+        {
+            //This should take in a config and test the connection to the database
+            SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            builder["Data Source"] = currentConfig.DbServer + "," + currentConfig.DbPort;
+            builder["integrated Security"] = false;
+            builder["ID"] = currentConfig.UserDb;
+            builder["Password"] = currentConfig.PasswordDb;
+            builder["Initial Catalog"] = currentConfig.DbName;
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch (SqlException)
+                {
+                    return false;
+                }
+            }
+
         }
     }
 }
